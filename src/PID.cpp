ec113705b55f82f6d7e8883cbd24962f8804d67c
sujigrena <sujigrena@gmail.com>
2018-04-19 01:28:21 +0530

    initial commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,20 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp1, double Ki1, double Kd1) {
+
+	Kp=Kp1;
+	Ki=Ki1;
+	Kd=Kd1;
 }
 
 void PID::UpdateError(double cte) {
+	d_error=cte-p_error;
+	p_error=cte;
+	i_error+=cte;
 }
 
 double PID::TotalError() {
+	return -(Kp*p_error)-(Ki*i_error)-(Kd*d_error);
 }
 
